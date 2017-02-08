df33377a559c1664f50fb5d8c98d1efdd588c5b5
fpli <952241861@qq.com>
2017-02-08 15:11:25 +0800

    编辑.podspec

---
+++ TRZXWebViewCategory/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TRZXWebViewCategory
+//
+//  Created by N年後 on 2017/2/8.
+//  Copyright © 2017年 TRZX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
